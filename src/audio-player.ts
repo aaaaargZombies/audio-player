a5eae2faf94c04e7608589bdbc032115c1cdc5ad
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-05-24 19:48:04 +0100

    first effectful actions
    - change plat/paused state
    - update ui to state
    - show warning message if missing audio element

--- src/audio-player.ts
+++ src/audio-player.ts
@@ -1,17 +1,11 @@
-/**
- * @license
- * Copyright 2019 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {customElement, state} from 'lit/decorators.js';
 
 /**
- * An example element.
+ * audio-player element.
  *
  * @fires play-changed - Indicates when the count changes
- * @slot - This element has a slot
+ * @slot {HTMLAudioElement} - add an `audio` element with the controlls attribute set pointing to the source you want to play. If the web-component fails to render you'll still be left with the browsers native audio player
  * @csspart button - The button
  */
 @customElement('audio-player')
@@ -25,33 +19,46 @@ export class AudioPlayer extends LitElement {
     }
   `;
 
-  @property()
-  _audio = null;
+  private _audio: HTMLAudioElement | null = null;
+
+  @state()
+  private _paused: boolean = true;
 
   override connectedCallback(): void {
+    super.connectedCallback();
     this._audio = this.querySelector('audio');
     console.log(this._audio);
   }
 
   override render() {
-    return html`<h1>Hello</h1>
-      <button @click="${this._onClick}" part="button"></button>
-      <slot></slot>`;
+    const missingAudioMsg = html`<div>
+      <p>oops - we can't find the track you were looking for</p>
+    </div>`;
+
+    const audioControls = html`<h1><code>audio-player</code></h1>
+      <button @click="${this._onClick}" part="button">
+        ${this._paused ? html`play` : html`pause`}
+      </button>`;
+
+    return html`
+      ${this._audio ? audioControls : missingAudioMsg}
+      <slot></slot>
+    `;
   }
 
   private _onClick() {
+    console.log('hellow');
+    console.log(this._audio);
     if (this._audio) {
-      this._audio.paused = !this._audio.paused;
+      if (this._audio.paused) {
+        this._audio.play();
+      } else {
+        this._audio.pause();
+      }
+      this._paused = !this._paused;
       this.dispatchEvent(new CustomEvent('play-changed'));
+      this.requestUpdate();
     }
-  }
-
-  /**
-   * Formats a greeting
-   * @param name The name to say "Hello" to
-   */
-  sayHello(name: string): string {
-    return `Hello, ${name}`;
   }
 }
 
