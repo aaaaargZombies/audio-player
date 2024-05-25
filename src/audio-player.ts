3bb78700a3366e1b386ddc6fce61570e7863eccf
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-05-25 13:28:25 +0100

    WIP trying to get some data from the audio into pixels, trouble grabbing the canvas element.

--- src/audio-player.ts
+++ src/audio-player.ts
@@ -1,5 +1,5 @@
 import {LitElement, html, css} from 'lit';
-import {customElement, state} from 'lit/decorators.js';
+import {customElement, property, query, state} from 'lit/decorators.js';
 
 /**
  * audio-player element.
@@ -21,13 +21,72 @@ export class AudioPlayer extends LitElement {
 
   private _audio: HTMLAudioElement | null = null;
 
+  private _analyser: AnalyserNode | null = null;
+
+  @query('canvas')
+  _canvas: HTMLCanvasElement | null;
+
   @state()
   private _paused: boolean = true;
+
+  @state()
+  private _wave: Uint8Array = new Uint8Array();
 
   override connectedCallback(): void {
     super.connectedCallback();
     this._audio = this.querySelector('audio');
-    console.log(this._audio);
+    [...this.shadowRoot?.children].forEach((element) => {
+      console.log(element);
+    });
+    if (this._audio) {
+      this._audio.controls = false;
+      let audioCtx = new window.AudioContext();
+      this._analyser = audioCtx.createAnalyser();
+      this._analyser.minDecibels = -90;
+      this._analyser.maxDecibels = -10;
+      this._analyser.smoothingTimeConstant = 0.85;
+
+      let source = audioCtx.createMediaElementSource(this._audio);
+      source.connect(this._analyser);
+      source.connect(audioCtx.destination);
+
+      this._analyser.fftSize = 256;
+      let bufferLength = this._analyser.frequencyBinCount;
+      this._wave = new Uint8Array(bufferLength);
+
+      let canvasCtx = this._canvas.getContext('2d');
+      if (canvasCtx) {
+        const draw = () => {
+          const height = this._canvas.height;
+          const width = this._canvas.width;
+          const drawVisual = requestAnimationFrame(draw);
+          canvasCtx.fillStyle = 'rgb(200 200 200)';
+          canvasCtx.fillRect(0, 0, width, height);
+          canvasCtx.lineWidth = 2;
+          canvasCtx.strokeStyle = 'rgb(0 0 0)';
+          canvasCtx.beginPath();
+          const sliceWidth = width / bufferLength;
+          let x = 0;
+          for (let i = 0; i < bufferLength; i++) {
+            const v = this._wave[i] / 128.0;
+            const y = v * (height / 2);
+
+            if (i === 0) {
+              canvasCtx.moveTo(x, y);
+            } else {
+              canvasCtx.lineTo(x, y);
+            }
+
+            x += sliceWidth;
+          }
+          canvasCtx.lineTo(width, height / 2);
+          canvasCtx.stroke();
+        };
+
+        canvasCtx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+        draw();
+      }
+    }
   }
 
   override render() {
@@ -38,7 +97,8 @@ export class AudioPlayer extends LitElement {
     const audioControls = html`<h1><code>audio-player</code></h1>
       <button @click="${this._onClick}" part="button">
         ${this._paused ? html`play` : html`pause`}
-      </button>`;
+      </button>
+      <canvas width="300" height="100"></canvas>`;
 
     return html`
       ${this._audio ? audioControls : missingAudioMsg}
@@ -52,6 +112,10 @@ export class AudioPlayer extends LitElement {
     if (this._audio) {
       if (this._audio.paused) {
         this._audio.play();
+        setTimeout(() => {
+          this._analyser.getByteTimeDomainData(this._wave);
+          console.log(this._wave);
+        }, 2000);
       } else {
         this._audio.pause();
       }
